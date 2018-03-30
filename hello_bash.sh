0ec8a0230cd6f2d80bb245e40f900713d99b4f84
ungorillaimo <ungorillaimo@gmail.com>
2018-03-30 23:03:59 +0900

    Add new shellscript

---
+++ hello_bash.sh
@@ -0,0 +1,29 @@
+echo "hello bash!!!"
+echo "I'm Kaka!"
+echo "Nice to meet you!"
+
+for i in u n g o r i l l a
+do
+	echo $i
+done
+
+echo "今から行くね"
+
+read yukan_no_shonen
+while [ "$yukan_no_shonen" != "やだ" ]
+do
+echo "行くね!"
+read yukan_no_shonen
+done
+
+echo "行　　　　く　　　　　　ね"
+
+read yukan_no_shonen2
+while [ "$yukan_no_shonen2" != "いいよ" ]
+do
+echo "行くね　　　　　　行く　ね　　行　くね行　くね　　　　　　　　　　行　く　ね　行く　　　　　　ね行く　ね　　　　　　　　　　　　ねね　ね　　　　　　　ねねねねねねね　ねねねねねねねねネネネネネネネネネネネネネネネネネネネネネネネネネネネネネネネネネネネネ"
+read yukan_no_shonen2
+done
+
+echo "あナたの　うシロニ"
+echo "　　　　　　　　　　　　　　　　　　イ　る　　ヨ。"
